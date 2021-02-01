9ee7e13b8b9c1fae0fd7f305ef41722f4e8f4003
naveen rawat <naveen@complitech.net>
2021-02-01 18:45:19 +0530

    change status code

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -7,12 +7,12 @@ class Api::Users::SessionsController < Devise::SessionsController
       if @user.valid_password?(params[:user][:password])
         token = JsonWebToken.encode(user_id: @user.id)
         @user.update_column('auth_token', token)
-        render json: {message: "User Login Successfully.", user: UserSerializer.new(@user, root: false), status: 202}, status: 200
+        render json: {message: "User Login Successfully.", user: UserSerializer.new(@user, root: false), status: 201}, status: 200
       else
         render json: {message: "Wrong password. Could not authenticate!", error: "Wrong Password", status: 401}, status: 200
       end
     else
-      render json: {message: "User does not exist with this email.", error: 'User does not exist.', status: 404 }, status: 200
+      render json: {message: "User does not exist with this mail.", error: 'User does not exist.', status: 404 }, status: 200
     end
   end
 end
