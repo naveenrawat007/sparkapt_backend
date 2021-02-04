5fd0092e1c3efd4141c71b2df0a521d1e4aef87a
naveen rawat <naveen@complitech.net>
2021-02-04 14:11:07 +0530

    set domain for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,19 +57,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
 # change to true to allow email to be sent during development
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.default :charset => "utf-8"
-   config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
-   port: 587,
-   domain: "example.com",
-   authentication: "plain",
-   enable_starttls_auto: true,
-   user_name: "rawatnaveennr123@gmail.com" ,
-   password: "naveen@28"
+  config.action_mailer.default_url_options = { :host => "192.53.163.52" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "rawatnaveennr123@gmail.com" ,
+    password: "naveen@28"
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
