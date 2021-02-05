5e93c93876d53eae4c34c1dd408e36eb68190c46
naveen rawat <naveen@complitech.net>
2021-02-05 14:17:30 +0530

    change mailer

--- app/mailers/user_welcome_mailer.rb
+++ app/mailers/user_welcome_mailer.rb
@@ -2,13 +2,13 @@ class UserWelcomeMailer < ApplicationMailer
 
   def welcome(user)
     @user = User.find(user)
-    mail(to: [@user.email], subject: "Welcome to SmartApt… 😊")
+    mail(to: [@user.email], from:"admin@geniusaptsearch.com" ,subject: "Welcome to SmartApt… 😊")
 	end
 
   def forget_password(user, domain)
     @user = User.find(user)
     @domain = domain
-    mail(to: [@user.email], subject: "Forget Password 😊")
+    mail(to: [@user.email], from:"admin@geniusaptsearch.com" ,subject: "Forget Password 😊")
   end
 
 end
