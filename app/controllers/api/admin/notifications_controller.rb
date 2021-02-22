535f52cbe18711d0c7fcff57e6476917356b2b40
naveen rawat <naveen@complitech.net>
2021-02-22 18:18:10 +0530

    show last 50 notifications

--- app/controllers/api/admin/notifications_controller.rb
+++ app/controllers/api/admin/notifications_controller.rb
@@ -1,21 +1,17 @@
 module Api
   module Admin
     class NotificationsController < Api::MainController
-      before_action :authorize_admin_request
+      before_action :authorize_admin_request, except: :index
 
       def index
-        if @current_user
-          render json: { message: "Notifications.", status: 200, notifications: ActiveModelSerializers::SerializableResource.new(Notification.all.order(created_at: :asc), each_serializer: NotificationSerializer)} and return
-        else
-          render json: { message: "Admin not found", status: 400}
-        end
+        render json: { message: "Notifications.", status: 200, notifications: ActiveModelSerializers::SerializableResource.new(Notification.last(50), each_serializer: NotificationSerializer)} and return
       end
 
       def create
         if @current_user
           notification = Notification.create(notification_params)
           if notification.save
-            render json: { message: "Notification Create Sucessfully.", status: 200, notifications: ActiveModelSerializers::SerializableResource.new(Notification.all.order(created_at: :asc), each_serializer: NotificationSerializer)} and return
+            render json: { message: "Notification Create Sucessfully.", status: 200, notifications: ActiveModelSerializers::SerializableResource.new(Notification.last(50), each_serializer: NotificationSerializer)} and return
           else
             render json: { message: "Notification not created", status: 400} and return
           end
