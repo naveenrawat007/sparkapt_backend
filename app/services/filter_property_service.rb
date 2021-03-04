1c1cddcdc592759262eed29a63fe12fd5ab54be9
naveen rawat <naveen@complitech.net>
2021-03-04 12:36:59 +0530

    filter with search feature

--- app/services/filter_property_service.rb
+++ app/services/filter_property_service.rb
@@ -9,23 +9,23 @@ class FilterPropertyService
       city = City.find_by(id: @params[:city_id])&.name
       if city == "All"
         if @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present? && !@params[:commission].present?
-          properties = Property.all.price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).order(created_at: :asc)
+          properties = Property.all.price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).search_filter(@params[:search]).order(created_at: :asc)
         elsif @params[:commission].present? && !@params[:yearFilter][:to_year].present? && !@params[:yearFilter][:from_year].present?
-          properties = Property.all.price_filter(0, @params[:max_price]).escort_filter(@params[:commission].to_i).order(created_at: :asc)
+          properties = Property.all.price_filter(0, @params[:max_price]).escort_filter(@params[:commission].to_i).search_filter(@params[:search]).order(created_at: :asc)
         elsif @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present? && @params[:commission].present?
-          properties = Property.all.price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).escort_filter(@params[:commission].to_i).order(created_at: :asc)
+          properties = Property.all.price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).escort_filter(@params[:commission].to_i).search_filter(@params[:search]).order(created_at: :asc)
         else
-          properties = Property.all.price_filter(0, @params[:max_price]).order(created_at: :asc)
+          properties = Property.all.price_filter(0, @params[:max_price]).search_filter(@params[:search]).order(created_at: :asc)
         end
       else
         if @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present? && !@params[:commission].present?
-          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).order(created_at: :asc)
+          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).search_filter(@params[:search]).order(created_at: :asc)
         elsif @params[:commission].present? && !@params[:yearFilter][:to_year].present? && !@params[:yearFilter][:from_year].present?
-          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).escort_filter(@params[:commission].to_i).order(created_at: :asc)
+          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).escort_filter(@params[:commission].to_i).search_filter(@params[:search]).order(created_at: :asc)
         elsif @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present? && @params[:commission].present?
-          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).escort_filter(@params[:commission].to_i).order(created_at: :asc)
+          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).escort_filter(@params[:commission].to_i).search_filter(@params[:search]).order(created_at: :asc)
         else
-          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).order(created_at: :asc)
+          properties = Property.where(city_id: @params[:city_id].to_i).price_filter(0, @params[:max_price]).search_filter(@params[:search]).order(created_at: :asc)
         end
       end
       OpenStruct.new(message: 'Properties.', status: 200, properties: ActiveModelSerializers::SerializableResource.new(properties, each_serializer: PropertySerializer))
