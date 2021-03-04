6e937fccc1290eba64d1429e24f5865caa7f46f9
naveen rawat <naveen@complitech.net>
2021-03-04 11:27:11 +0530

    create scopes for filters

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,5 +7,6 @@ class Property < ApplicationRecord
   scope :price_filter, ->(min,max) { where('price >= ? AND price <= ?', min, max) }
   scope :built_year_filter, ->(from_year,to_year) { where('built_year >= ? AND built_year <= ?', from_year, to_year) }
   scope :escort_filter, -> (escort_percent) {where("escort > ?", escort_percent)}
+  scope :search_filter, -> (search_str) {where("lower(name) LIKE :search OR lower(phone) LIKE :search OR lower(email) LIKE :search OR cast(price AS TEXT) LIKE :search OR cast(built_year AS TEXT) LIKE :search OR lower(manager_name) LIKE :search OR lower(management_company) LIKE :search OR cast(escort AS TEXT) LIKE :search", search: "%#{search_str.downcase}%")} 
 
 end
