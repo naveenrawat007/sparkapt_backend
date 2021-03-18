7d185b65c45cc8856f69301477b3b69c74fbebb3
naveen rawat <naveen@complitech.net>
2021-03-18 10:12:28 +0530

    validate if same name property is present

--- app/services/import_property_service.rb
+++ app/services/import_property_service.rb
@@ -10,7 +10,7 @@ class ImportPropertyService
     @params[:properties].each_with_index do |data, index|
 
       if index > 1
-        if data[0].present?
+        if data[0].present? && !Property.find_by(name: data[0].to_s).present?
           property = Property.create(name: data[0].to_s,phone: data[1].to_s, specials: data[2].to_s, submarket: data[3].to_s, zip: data[4].to_s, built_year: data[5].to_i, escort: data[6].to_f * 100, management_company: data[19].to_s, web_link: data[20].to_s, google_map: data[21].to_s, manager_name: data[22].to_s, email: data[23].to_s, google_rating: data[24].to_s, lat: data[25].to_s.split.first.to_f, long: data[25].to_s.split.last.to_f, address: data[26].to_s, city_id: @city_id)
 
           @params[:properties][0].each do |property_type|
