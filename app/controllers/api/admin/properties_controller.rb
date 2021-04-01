0208b4d29bf1422e36a2db4f3f1c7cc2f75b452d
naveen rawat <naveen@complitech.net>
2021-04-01 18:21:11 +0530

    check for empty value

--- app/controllers/api/admin/properties_controller.rb
+++ app/controllers/api/admin/properties_controller.rb
@@ -123,8 +123,10 @@ module Api
               property_data["property_type_details"].each do |type_detail|
                 type = Type.find_by(name: type_detail['property_type'])
                 if type
-                  property_type_detail = type.type_details.create(notes: type_detail['notes'], price: type_detail['price'], available: type_detail['available'], floor_plan: type_detail['floor_plan'], size: type_detail['size'], floor_plan: type_detail['floor_plan'], property_type_name: type_detail['property_type'])
-                  property_type_detail.update(property_id: property&.id)
+                  if type_detail['price'] != ""
+                    property_type_detail = type.type_details.create(notes: type_detail['notes'], price: type_detail['price'], available: type_detail['available'], floor_plan: type_detail['floor_plan'], size: type_detail['size'], floor_plan: type_detail['floor_plan'], property_type_name: type_detail['property_type'])
+                    property_type_detail.update(property_id: property&.id)
+                  end
                 else
                   render json: { message: "PropertyType not Found.", status: 400} and return
                 end
