e3e699f81280cabdff538eda8304bd3a02af970f
naveen rawat <naveen@complitech.net>
2021-04-10 13:56:39 +0530

    remove unwanted code

--- app/mailers/user_welcome_mailer.rb
+++ app/mailers/user_welcome_mailer.rb
@@ -2,13 +2,13 @@ class UserWelcomeMailer < ApplicationMailer
 
   def welcome(user)
     @user = User.find(user)
-    mail(to: [@user.email], from:"info@goodlifelocating.com" ,subject: "Welcome to SparkAPT… 😊")
+    mail(to: [@user.email] ,subject: "Welcome to SparkAPT… 😊")
 	end
 
   def forget_password(user, domain)
     @user = User.find(user)
     @domain = domain
-    mail(to: [@user.email], from:"info@goodlifelocating.com" ,subject: "Forget Password")
+    mail(to: [@user.email] ,subject: "Forget Password")
   end
 
   def contact_inquiry(id)
@@ -19,13 +19,13 @@ class UserWelcomeMailer < ApplicationMailer
   def account_approve(user_id)
     @user = User.find(user_id)
     @admin_mail = User.find_by(is_admin: true)&.email
-    mail(to: @admin_mail, from:"info@goodlifelocating.com" ,subject: "New User Added: Needs Approval")
+    mail(to: @admin_mail,subject: "New User Added: Needs Approval")
   end
 
   def property_report(code, email, domain)
     @domain = domain
     @code = code
-    mail(to: email, from:"info@goodlifelocating.com" ,subject: "Property Report Detail")
+    mail(to: email ,subject: "Property Report Detail")
   end
 
 end
