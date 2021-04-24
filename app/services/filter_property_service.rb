ae3fb759b32287d1069b0560a44093d8b0cacf45
naveen rawat <naveen@complitech.net>
2021-04-24 10:59:53 +0530

    refactor filter scopes

--- app/services/filter_property_service.rb
+++ app/services/filter_property_service.rb
@@ -9,10 +9,14 @@ class FilterPropertyService
   end
 
   def call
-    if @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present?
+    if @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present? && @params[:sq_ft] != ""
       properties = @properties.price_filter(0, @params[:max_price]).sq_feet_filter(@sq_feet, @params[:show_property_type]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).escort_filter(@commission).send_escort_filter(@send_commisson).search_filter(@params[:search]).order(created_at: :asc)
-    else
+    elsif @params[:yearFilter][:to_year].present? && @params[:yearFilter][:from_year].present? && @params[:sq_ft] == ""
+      properties = @properties.price_filter(0, @params[:max_price]).built_year_filter(@params[:yearFilter][:from_year].to_i, @params[:yearFilter][:to_year].to_i).escort_filter(@commission).send_escort_filter(@send_commisson).search_filter(@params[:search]).order(created_at: :asc)
+    elsif @params[:sq_ft] != ""
       properties = @properties.price_filter(0, @params[:max_price]).sq_feet_filter(@sq_feet, @params[:show_property_type]).escort_filter(@commission).send_escort_filter(@send_commisson).search_filter(@params[:search]).order(created_at: :asc)
+    else
+      properties = @properties.price_filter(0, @params[:max_price]).escort_filter(@commission).send_escort_filter(@send_commisson).search_filter(@params[:search]).order(created_at: :asc)
     end
 
     OpenStruct.new(message: 'Properties.', status: 200, properties: ActiveModelSerializers::SerializableResource.new(properties.uniq, each_serializer: PropertySerializer))
